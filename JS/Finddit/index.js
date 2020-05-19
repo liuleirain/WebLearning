2ac25aafdce26bbd4eab1bb247406a2a83d52d31
asdfllzxcv <14699396@qq.com>
2020-05-19 14:12:56 +0800

    finish

--- JS/Finddit/index.js
+++ JS/Finddit/index.js
@@ -21,7 +21,33 @@ searchForm.addEventListener('submit', (e) => {
   searchInput.value = '';
 
   // Search Reddit
-  reddit.search(searchTerm, searchLimit, sortBy);
+  reddit.search(searchTerm, searchLimit, sortBy).then((results) => {
+    console.log(results);
+    let output = '<div class="card-columns">';
+
+    // Loop through posts
+    results.forEach((post) => {
+      // Check for image
+      const image = post.preview
+        ? post.preview.images[0].source.url
+        : 'https://www.affiliatemarketertraining.com/wp-content/uploads/2015/01/Reddit.jpg';
+      output += `
+      <div class="card">
+      <img class="card-img-top" src="${image}" alt="Card image cap">
+      <div class="card-body">
+      <h5 class="card-title">${post.title}</h5>
+      <p class="card-text">${truncateText(post.selftext, 100)}</p>
+      <a href="${post.url}" class="btn btn-primary">Read More</a>
+      <hr>
+      <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+      <span class="badge badge-dark">Score: ${post.score}</span>
+      </div>
+      </div>
+      `;
+    });
+    output += '</div>';
+    document.getElementById('results').innerHTML = output;
+  });
 
   e.preventDefault();
 });
@@ -44,4 +70,11 @@ function showMessage(message, className) {
 
   // Timeout alert
   setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
+
+// Truncate Text
+function truncateText(text, limit) {
+  const shortened = text.indexOf(' ', limit);
+  if (shortened == -1) return text;
+  return text.substring(0, shortened);
 }
